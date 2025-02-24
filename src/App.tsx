ae3c9b30f9a87b7a0d9ce17da1e1382547746779
shifatAhmed <shifatahmed410@gmail.com>
2025-02-24 23:24:04 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import Message from "./Message"
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  let[todolist,setTodolist]=useState([])
+
+  let saveToDoList=(event)=>{
+    
+    let todoname= event.target.todoname.value;
+    if(!todolist.includes(todoname)){
+      let allTodolist = [...todolist,todoname]
+      setTodolist(allTodolist);
+    }
+    else{
+      alert('item already exits..');
+    }
+    event.preventDefault();
+
+  }
+
+  let list=todolist.map((value,index)=>{
+
+    return(
+      <TodoItems value={value} key={index} indexNum={index}
+      todolist={todolist} setTodolist={setTodolist}/>
+    )
+
+  })
+
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type="text"  name="todoname"/><button>ADD</button>
+      </form>
+      <div className="ulDiv">
+        <ul>
+        {list}
+        </ul>
+      </div>
+      
+    </div>
+    
+  )
+}
+
+export default App
+
+function TodoItems({value,indexNum,todolist,setTodolist}){
+  let [status,setStatus]=useState(false)
+  let deleteItem=()=>{
+    let finalList=todolist.filter((v,i)=>i!=indexNum)
+    setTodolist(finalList);
+  }
+  
+  let changeStatus=()=>{
+    setStatus(!status)
+  }
+  return(
+    <li className ={(status)? 'todoDone': ''}onClick={changeStatus}>{value} <span onClick={deleteItem}>&times;</span></li>
+  )
+}
---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+function Message(){ 
+    const name = 'shifat';
+    return <h1>hello {name}</h1>;
+}
+export default Message
